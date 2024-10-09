762f7df59c1c41e67d1a90cbf48ba7b6113486d1
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-09 23:19:41 +0900

    realmのobject利用

--- composeApp/src/commonMain/kotlin/repository/count/CountRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/repository/count/CountRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package repository.count
 
-
+import domain.Counter
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -11,11 +14,26 @@ class CountRepositoryImpl : CountRepository {
         replay = 1,
     )
 
-    override var count: Int = 0
+    private val config: RealmConfiguration =
+        RealmConfiguration.create(schema = setOf(Counter::class))
+    private val realm: Realm = Realm.open(config)
+    private val counter: Counter =
+        realm.query<Counter>().first().find() ?: realm.writeBlocking {
+            copyToRealm(
+                Counter()
+            )
+        }
+
+    override var count: Int = counter.count
         set(value) {
             field = value
             CoroutineScope(Dispatchers.Default).launch {
                 mutableCountFlow.emit(value)
+            }
+            realm.writeBlocking {
+                findLatest(counter)?.apply {
+                    count = value
+                }
             }
         }
 }
