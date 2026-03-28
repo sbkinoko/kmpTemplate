fefc3cf0d83e5d7935b81903180da433e3de9f85
sbknk <136720653+sbkinoko@users.noreply.github.com>
2026-03-28 20:47:19 +0900

    version更新

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
+
+    androidTarget()
 
     jvm("desktop")
 
@@ -37,12 +33,12 @@ kotlin {
             implementation(libs.koin.android)
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.runtime)
+            implementation(libs.foundation)
+            implementation(libs.material)
+            implementation(libs.ui)
+            implementation(libs.components.resources)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
         }
